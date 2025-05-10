1d7763cd3f24881f66d0f17fe0b1d13c710061fb
heechul <khc4190@naver.com>
2025-05-10 14:09:40 +0900

    조회수 어뷰징 방지 정책 구현 - redis의 setIfAbsent 사용

--- service/view/src/main/java/study/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/study/board/view/service/ArticleViewService.java
@@ -3,15 +3,23 @@ package study.board.view.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import study.board.view.repository.ArticleViewCountRepository;
+import study.board.view.repository.ArticleViewDistributedLockRepository;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
     private final ArticleViewCountRepository articleViewCountRepository;
     private static final int BACKUP_BATCH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10);
 
     public Long increase(Long articleId, Long userId) {
+        if(!articleViewDistributedLockRepository.lock(articleId, userId, TTL)){
+            return articleViewCountRepository.read(articleId);
+        }
         Long count = articleViewCountRepository.increase(articleId);
         if (count % BACKUP_BATCH_SIZE == 0) {
             articleViewCountBackUpProcessor.backUp(articleId, count);
