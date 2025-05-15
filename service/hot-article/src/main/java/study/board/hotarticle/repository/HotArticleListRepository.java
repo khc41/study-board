035100b3f9f92ca9a9503595a4f4fc80970342af
heechul <khc4190@naver.com>
2025-05-15 23:03:23 +0900

    이벤트 데이터 리포지토리 구현

--- service/hot-article/src/main/java/study/board/hotarticle/repository/HotArticleListRepository.java
+++ service/hot-article/src/main/java/study/board/hotarticle/repository/HotArticleListRepository.java
@@ -25,21 +25,25 @@ public class HotArticleListRepository {
     private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
 
     public void add(Long articleId, LocalDateTime time, Long score, Long limit, Duration ttl) {
-        redisTemplate.executePipelined((RedisCallback<?>) action ->{
+        redisTemplate.executePipelined((RedisCallback<?>) action -> {
             StringRedisConnection conn = (StringRedisConnection) action;
             String key = generateKey(time);
             conn.zAdd(key, score, String.valueOf(articleId));
-            conn.zRemRange(key, 0, - limit - 1);
+            conn.zRemRange(key, 0, -limit - 1);
             conn.expire(key, ttl.toSeconds());
             return null;
         });
     }
 
-    private String generateKey(LocalDateTime time){
+    public void remove(Long articleId, LocalDateTime time) {
+        redisTemplate.opsForZSet().remove(generateKey(time), String.valueOf(articleId));
+    }
+
+    private String generateKey(LocalDateTime time) {
         return generateKey(TIME_FORMATTER.format(time));
     }
 
-    private String generateKey(String dateStr){
+    private String generateKey(String dateStr) {
         return KEY_FORMAT.formatted(dateStr);
     }
 
