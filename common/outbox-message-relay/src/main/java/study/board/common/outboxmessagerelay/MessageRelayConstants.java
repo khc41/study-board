9db0fe70e4849b76701f4e4e86e7f9c4899fee4f
heechul <khc4190@naver.com>
2025-06-01 15:57:06 +0900

    transactional outbox 모듈 구현

---
+++ common/outbox-message-relay/src/main/java/study/board/common/outboxmessagerelay/MessageRelayConstants.java
@@ -0,0 +1,8 @@
+package study.board.common.outboxmessagerelay;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public class MessageRelayConstants {
+    public static final int SHARD_COUNT = 4; // 임의의 값
+}
