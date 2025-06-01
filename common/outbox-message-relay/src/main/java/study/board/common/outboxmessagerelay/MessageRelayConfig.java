6685f9c47505911f927347ab4da02f0bab3fb99c
heechul <khc4190@naver.com>
2025-06-01 16:32:02 +0900

    transactional outbox 모듈 버그 수정

--- common/outbox-message-relay/src/main/java/study/board/common/outboxmessagerelay/MessageRelayConfig.java
+++ common/outbox-message-relay/src/main/java/study/board/common/outboxmessagerelay/MessageRelayConfig.java
@@ -1,6 +1,7 @@
 package study.board.common.outboxmessagerelay;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -28,8 +29,8 @@ public class MessageRelayConfig {
     public KafkaTemplate<String, String> messageRelayKafkaTemplate() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, String.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, String.class);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.ACKS_CONFIG, "all");
         return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(configProps));
     }
